7c6ad3ad16125c73a2df486f0eaf4510e340f948
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-07 17:18:02 -0300

    frist commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/lib/ultis'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Teste Tupiniquim',
+  description: 'Teste front end para agencia tupiniquim',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-br" className="!scroll-smooth">
+      <body className={cn(inter.className, 'font-sans antialiased')}>
+        <main className="relative flex flex-col min-h-dvh overflow-hidden">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
