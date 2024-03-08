a06b611965b9959dbff3d64697e2fb4034aa5026
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-08 19:26:03 -0300

    feat: welcome section

--- components/InfoSection.tsx
+++ components/InfoSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function InfoSection() {
@@ -12,18 +13,31 @@ export default function InfoSection() {
       }}
     >
       <div className="w-full max-w-screen-xl px-2.5 md:px-6 h-[50vh] sm:h-[60vh] md:h-[80vh] lg:h-[130vh]">
-        <div className="h-1/2 w-3/5">
-          <p className="mb-5 text-neutral">
-            Driveon is a mobile carpool platform that effortlessly brings
-            commuters together in one car. Traveling home and work will never be
-            he same again. Our mission is to stop traffic jams and CO2 pollution
-            as a result of inefficient car commuting
-          </p>
-          <span>Are you in?</span>
-          <Link href={'/'} className="link">
-            Learn more
-          </Link>
+        <div className="flex justify-end w-full h-1/2">
+          <div className="w-3/5 text-neutral text-3xl">
+            <Image
+              className="py-16"
+              src="/linha-reta.jpg"
+              alt="linha reta"
+              width={150}
+              height={100}
+            />
+            <p className="">
+              Driveon is a mobile carpool platform that effortlessly brings
+              commuters together in one car.
+            </p>
+            <p>
+              Traveling home and work will never be he same again. Our mission
+              is to stop traffic jams and CO2 pollution as a result of
+              inefficient car commuting
+            </p>
+            <p>Are you in?</p>
+            <Link href={'/'} className="link">
+              Learn more
+            </Link>
+          </div>
         </div>
+
         <div className="h-1/2 w-3/5">
           <h3>Our commitment to your safely</h3>
           <p className="mb-5 text-neutral">
