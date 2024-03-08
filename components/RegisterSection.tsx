1073e692d840339d6944873d97e2993aaac136a2
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-08 10:47:01 -0300

    feat: register section

---
+++ components/RegisterSection.tsx
@@ -0,0 +1,12 @@
+export default function RegisterSection() {
+  return (
+    <div className="mx-auto w-full max-w-screen-2xl px-2.5 md:px-6">
+      <div>
+        <h3>Register to be a driver partner</h3>
+      </div>
+      <div>
+        <h3>Register to start your journey</h3>
+      </div>
+    </div>
+  )
+}
