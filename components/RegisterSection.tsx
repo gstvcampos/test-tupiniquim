b182ae3cb5df7ac03b5c1da6a4e85f386d5f12c2
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-08 17:56:27 -0300

    feat: register section

--- components/RegisterSection.tsx
+++ components/RegisterSection.tsx
@@ -1,11 +1,26 @@
+import Image from 'next/image'
+
 export default function RegisterSection() {
   return (
-    <div className="mx-auto w-full max-w-screen-2xl px-2.5 md:px-6">
-      <div>
-        <h3>Register to be a driver partner</h3>
-      </div>
-      <div>
-        <h3>Register to start your journey</h3>
+    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-6">
+      <div className="flex">
+        <div className="w-1/2">
+          <h3>Register to be a driver partner</h3>
+          <div>
+            <Image src="/seta.png" alt="seta" width={70} height={70} />
+          </div>
+          <div>
+            <Image
+              src="/linha-reta.jpg"
+              alt="linha reta"
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+        <div className="w-1/2">
+          <h3>Register to start your journey</h3>
+        </div>
       </div>
     </div>
   )
