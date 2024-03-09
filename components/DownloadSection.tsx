ca082f6e0b92d07eafe228c0dc66d8f82f3a1322
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-09 12:35:01 -0300

    feat: responsive info section

--- components/DownloadSection.tsx
+++ components/DownloadSection.tsx
@@ -4,39 +4,42 @@ import { PlayAppIcon } from './icons/PlayAppIcon'
 
 export default function DownloadSection() {
   return (
-    <div
-      className="hero"
-      style={{
-        backgroundImage: 'url(downloadImage.png)',
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-      }}
-    >
-      <div className="w-full h-[400px] md:h-[700px] lg:h-[950px]">
-        <div className="max-w-screen-xl mx-auto h-full w-full flex justify-end">
-          <div className="w-3/5 h-full flex flex-col justify-center">
-            <Image
-              className="py-8"
-              src="/linha-white.png"
-              alt="linha reta"
-              width={150}
-              height={100}
-            />
-            <h2 className="font-bold text-2xl text-primary">
-              Download the DriveOn application to get more benefits and
-              experience
-            </h2>
-            <div>
-              <button className="btn btn-primary">
-                <ClayStoreIcon />
-                Clay Store
-              </button>
-              <button className="btn btn-primary">
-                <PlayAppIcon />
-                Download on the Play APP
-              </button>
-            </div>
+    <div className="w-full h-[400px] md:h-[700px] lg:h-[950px] sm:bg-[url('/downloadImage.png')] bg-contain bg-no-repeat bg-center">
+      <div className="max-w-screen-xl mx-auto h-full w-full flex justify-center sm:justify-end">
+        <div className="md:w-2/4 h-full flex flex-col justify-center">
+          <Image
+            className="hidden sm:block py-8"
+            src="/linha-white.png"
+            alt="linha reta"
+            width={150}
+            height={100}
+          />
+          <Image
+            className="py-8 sm:hidden"
+            src="/linha-reta.jpg"
+            alt="linha reta"
+            width={150}
+            height={100}
+          />
+          <h2 className="font-bold text-2xl text-primary py-8">
+            Download the DriveOn application <br />
+            to get more benefits and experience
+          </h2>
+          <div className="space-x-10">
+            <button className="btn bg-black text-white border-none hover:bg-black">
+              <ClayStoreIcon />
+              <p className="flex flex-col items-start">
+                <span className="font-normal text-xs">GET IT ON</span>
+                <span className="text-lg">Clay Store</span>
+              </p>
+            </button>
+            <button className="btn bg-black text-white border-none hover:bg-black">
+              <PlayAppIcon />
+              <p className="flex flex-col items-start">
+                <span className="font-normal text-xs">Download on the</span>
+                <span className="text-lg">Play APP</span>
+              </p>
+            </button>
           </div>
         </div>
       </div>
