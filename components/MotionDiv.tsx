f57781ce6100fc725ecae0f7c1c66a8b7190aaf4
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-11 08:04:17 -0300

    feat: frist animation only

--- components/MotionDiv.tsx
+++ components/MotionDiv.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion, useInView } from 'framer-motion'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 export default function MotionDiv({
   children,
@@ -12,13 +12,18 @@ export default function MotionDiv({
 }) {
   const ref = useRef(null)
   const isInView = useInView(ref)
+  const [hasAnimated, setHasAnimated] = useState(false)
+
+  if (isInView && !hasAnimated) {
+    setHasAnimated(true)
+  }
 
   return (
     <motion.div
       ref={ref}
       className={className}
       initial={{ y: 80, opacity: 0 }}
-      animate={{ y: isInView ? 0 : 80, opacity: isInView ? 1 : 0 }}
+      animate={{ y: hasAnimated ? 0 : 80, opacity: hasAnimated ? 1 : 0 }}
       transition={{ ease: 'easeInOut', duration: 0.75 }}
     >
       {children}
